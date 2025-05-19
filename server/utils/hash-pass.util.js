e0998366022af2b131623f94d5fbb97d6369d2fd
Dossman-thomas <tdossman.development@gmail.com>
2025-05-19 15:09:15 -0400

    created test runner for hash pass so I can update an admin users password for login testing purposes.

--- server/utils/hash-pass.util.js
+++ server/utils/hash-pass.util.js
@@ -5,3 +5,19 @@ export const hashPassword = async (password) => {
   const hashedPassword = await bcrypt.hash(password, salt);
   return hashedPassword;
 };
+
+// Temporary test runner
+// if (process.argv[2]) {
+//   const testPassword = process.argv[2];
+//   hashPassword(testPassword).then((hashed) => {
+//     console.log(`Plain: ${testPassword}`);
+//     console.log(`Hashed: ${hashed}`);
+//   });
+// }
+
+// Run test with node server/utils/hash-pass.util.js <password>
+
+// Example: node server/utils/hash-pass.util.js Test1234!
+// Output: 
+// Plain: Test1234!
+// Hashed: $2b$10$HiKs5dq3l3xYeBuP/F/d0uQDX/XoZl5qhgz9lMhhvBTOhsftuu1qK
