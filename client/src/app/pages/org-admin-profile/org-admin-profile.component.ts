d389c4ad250ffc5063c2ac437586a71f33d5577d
Dossman-thomas <tdossman.development@gmail.com>
2025-06-06 07:16:22 -0400

    prefilled org admin profile fields with user and org data

--- client/src/app/pages/org-admin-profile/org-admin-profile.component.ts
+++ client/src/app/pages/org-admin-profile/org-admin-profile.component.ts
@@ -1,10 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CryptoService } from '../../services/crypto.service';
+import { OrganizationService } from '../../services/organization.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-org-admin-profile',
   templateUrl: './org-admin-profile.component.html',
-  styleUrls: ['./org-admin-profile.component.css']
+  styleUrls: ['./org-admin-profile.component.css'],
 })
-export class OrgAdminProfileComponent {
+export class OrgAdminProfileComponent implements OnInit {
+  orgProfileForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private organizationService: OrganizationService,
+    private userService: UserService,
+    private cryptoService: CryptoService
+  ) {}
+
+  ngOnInit(): void {
+    this.orgProfileForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      title: ['', Validators.required],
+      phoneNumber: [
+        '',
+        [Validators.required, Validators.pattern(/^[0-9\-()+ ]+$/)],
+      ],
+      address: ['', Validators.required],
+      orgName: ['', Validators.required],
+      website: [
+        '',
+        [Validators.required, Validators.pattern(/https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)],
+      ],
+      orgType: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+
+    this.loadProfileData();
+  }
+
+  private loadProfileData(): void {
+    try {
+      const encryptedUserId = localStorage.getItem('userId');
+      const encryptedOrgId = localStorage.getItem('orgId');
+
+      if (!encryptedUserId || !encryptedOrgId) {
+        console.error('Missing encrypted IDs in localStorage');
+        return;
+      }
+
+      const userId = this.cryptoService.Decrypt(encryptedUserId);
+      const orgId = this.cryptoService.Decrypt(encryptedOrgId);
+
+      this.userService.getUserById(userId).subscribe((userData) => {
+        this.orgProfileForm.patchValue({
+          firstName: userData.first_name || '',
+          lastName: userData.last_name || '',
+          title: userData.user_role || '',
+          phoneNumber: userData.user_phone_number || '',
+        });
+      });
+
+      this.organizationService.getOrganizationById(orgId).subscribe((orgData) => {
+        this.orgProfileForm.patchValue({
+          address: orgData.registeredAddress || '',
+          orgName: orgData.orgName || '',
+          website: orgData.website || '',
+          orgType: orgData.orgType || '',
+          email: orgData.orgEmail || '',
+        });
+      });
+
+    } catch (error) {
+      console.error('Error decrypting or fetching data:', error);
+    }
+  }
+
+  onSubmit(): void {
+    if (this.orgProfileForm.invalid) {
+      this.orgProfileForm.markAllAsTouched();
+      return;
+    }
+
+    const formData = this.orgProfileForm.value;
+    console.log('Submitting form data:', formData);
+    // add submit logic later
+  }
 }
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -13,36 +13,35 @@ export class UserService {
   private baseUrl = `${environment.backendUrl}/user`;
   constructor(private http: HttpClient, private cryptoService: CryptoService) {}
 
-// Update a user
-updateUser(payload: {
-  userId: string;
-  updatedBy: string;
-  first_name?: string;
-  last_name?: string;
-  user_phone_number?: string;
-  user_role?: string;
-}): Observable<any> {
-  const encryptedPayload = this.cryptoService.Encrypt(payload);
+  // Update a user
+  updateUser(payload: {
+    userId: string;
+    updatedBy: string;
+    first_name?: string;
+    last_name?: string;
+    user_phone_number?: string;
+    user_role?: string;
+  }): Observable<any> {
+    const encryptedPayload = this.cryptoService.Encrypt(payload);
 
-  return this.http
-    .put<any>(
-      `${this.baseUrl}/update`,
-      { payload: encryptedPayload },
-      { headers: getHeaders() }
-    )
-    .pipe(
-      catchError((error) =>
-        throwError(() => {
-          return (
-            error.error || {
-              message: 'Something went wrong while updating the user.',
-            }
-          );
-        })
+    return this.http
+      .put<any>(
+        `${this.baseUrl}/update`,
+        { payload: encryptedPayload },
+        { headers: getHeaders() }
       )
-    );
-}
-
+      .pipe(
+        catchError((error) =>
+          throwError(() => {
+            return (
+              error.error || {
+                message: 'Something went wrong while updating the user.',
+              }
+            );
+          })
+        )
+      );
+  }
 
   getUserByEmail(userEmail: string): Observable<any> {
     const payload = { user_email: userEmail };
@@ -64,22 +63,21 @@ updateUser(payload: {
   }
 
   getUserById(userId: string): Observable<any> {
-  const payload = { user_id: userId };
-  const encryptedPayload = this.cryptoService.Encrypt(payload);
+    const payload = { user_id: userId };
+    const encryptedPayload = this.cryptoService.Encrypt(payload);
 
-  return this.http
-    .post<any>(
-      `${this.baseUrl}/get-by-id`,
-      { payload: encryptedPayload },
-      { headers: getHeaders() }
-    )
-    .pipe(
-      map((response) => {
-        // Access the encrypted data from the response
-        const decryptedData = this.cryptoService.Decrypt(response.data);
-        return decryptedData;
-      })
-    );
-}
-
+    return this.http
+      .post<any>(
+        `${this.baseUrl}/get-by-id`,
+        { payload: encryptedPayload },
+        { headers: getHeaders() }
+      )
+      .pipe(
+        map((response) => {
+          // Access the encrypted data from the response
+          const decryptedData = this.cryptoService.Decrypt(response.data);
+          return decryptedData;
+        })
+      );
+  }
 }
